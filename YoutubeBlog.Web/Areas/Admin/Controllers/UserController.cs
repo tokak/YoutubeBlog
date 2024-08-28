896eb8ad4e41a45525ad90635c8e6e7df00b033e
Murat Tokak <tokakmurat01@gmail.com>
2024-08-28 18:16:30 +0300

    Tüm User'ları Rolleriyle beraber listeleme işlemleri

---
+++ YoutubeBlog.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using YoutubeBlog.Entity.DTOs.Users;
+using YoutubeBlog.Entity.Entities;
+
+namespace YoutubeBlog.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public UserController(UserManager<AppUser> userManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var map = _mapper.Map<List<UserDto>>(users);
+            foreach (var item in map)
+            {
+                var findUser = await _userManager.FindByIdAsync(item.Id.ToString());
+                var role = string.Join("",await _userManager.GetRolesAsync(findUser));
+
+                item.Role = role;
+            }
+            return View(map);
+        }
+    }
+}
